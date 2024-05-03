0a1a172f38ade726f67e07bfd54f735b26ce8234
chLi <565353780@qq.com>
2024-05-03 16:34:16 +0800

    update setup bash files

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
 pip install -U torch torchvision torchaudio
-pip install -U timm diffusers accelerate einops tensorboard
+pip install -U timm diffusers accelerate einops tensorboard \
+	open3d trimesh
